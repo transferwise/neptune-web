f75d6f68046e9a6b595c484d4f50ddcf8eea070d
Uku Markus Tammet <ukutammet@gmail.com>
2017-08-25 17:10:34 +0100

    Move documentation header to its own component

--- packages/components/docs/Docs.js
+++ packages/components/docs/Docs.js
@@ -1,63 +1,22 @@
-import React, { Component } from 'react';
-import marked from 'marked'; // eslint-disable-line import/no-extraneous-dependencies
-import npmPackage from '../package.json';
-import changelog from '../CHANGELOG.md';
+import React from 'react';
 import './Docs.css';
 
+import Header from './Header';
 import StepperDocs from './StepperDocs';
 import SelectDocs from './SelectDocs';
 import CheckboxDocs from './CheckboxDocs';
 import RadioDocs from './RadioDocs';
 import LoaderDocs from './LoaderDocs';
 
-export default class Docs extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { showChangelog: false };
-  }
+const Docs = () =>
+  <div className="container">
+    <Header />
 
-  render() {
-    return (
-      <div className="container">
-        <section className="section row">
-          <div className="col-md-6">
-            <h1>
-              Components 🎉 <small>v{npmPackage.version}</small>
-            </h1>
-            <p>TransferWise styleguide components in react.</p>
-            {/* eslint-disable react/jsx-indent */}
-            <pre className="tw-docs-code">
-              {'yarn add @transferwise/components'}
-            </pre>
-            {/* eslint-enable react/jsx-indent */}
-            <p>
-              <strong>TODO: </strong>
-              We need to add missing components and missing props to some components.
-            </p>
-            <button
-              className="btn btn-default"
-              onClick={() =>
-                this.setState(({ showChangelog }) => ({ showChangelog: !showChangelog }))}
-            >
-              {this.state.showChangelog ? 'Hide' : 'Show'} changelog
-            </button>
-          </div>
-          {/* eslint-disable react/no-danger */}
-          {this.state.showChangelog
-            ? <div
-              className="col-md-6 tw-docs-code"
-              dangerouslySetInnerHTML={{ __html: marked(atob(changelog.substring(28))) }}
-            />
-            : ''}
-          {/* eslint-enable react/no-danger */}
-        </section>
+    <StepperDocs />
+    <SelectDocs />
+    <CheckboxDocs />
+    <RadioDocs />
+    <LoaderDocs />
+  </div>;
 
-        <StepperDocs />
-        <SelectDocs />
-        <CheckboxDocs />
-        <RadioDocs />
-        <LoaderDocs />
-      </div>
-    );
-  }
-}
+export default Docs;
---
+++ packages/components/docs/Header.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import marked from 'marked'; // eslint-disable-line import/no-extraneous-dependencies
+import npmPackage from '../package.json';
+import changelog from '../CHANGELOG.md';
+
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showChangelog: false };
+  }
+
+  render() {
+    return (
+      <section className="section row">
+        <div className="col-md-6">
+          <h1>
+            Components 🎉 <small>v{npmPackage.version}</small>
+          </h1>
+          <p>TransferWise styleguide components in react.</p>
+          {/* eslint-disable react/jsx-indent */}
+          <pre className="tw-docs-code">
+            {'yarn add @transferwise/components'}
+          </pre>
+          {/* eslint-enable react/jsx-indent */}
+          <p>
+            <strong>TODO: </strong>
+            We need to add missing components and missing props to some components.
+          </p>
+          <button
+            className="btn btn-default"
+            onClick={() =>
+              this.setState(({ showChangelog }) => ({ showChangelog: !showChangelog }))}
+          >
+            {this.state.showChangelog ? 'Hide' : 'Show'} changelog
+          </button>
+        </div>
+        {/* eslint-disable react/no-danger */}
+        {this.state.showChangelog
+          ? <div
+            className="col-md-6 tw-docs-code"
+            dangerouslySetInnerHTML={{ __html: marked(atob(changelog.substring(28))) }}
+          />
+          : ''}
+        {/* eslint-enable react/no-danger */}
+      </section>
+    );
+  }
+}
