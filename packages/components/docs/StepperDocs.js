1e09afb3f57cbced78bdafec5af905b77739c17e
Uku Markus Tammet <ukutammet@gmail.com>
2017-08-28 12:46:14 +0300

    Add action preset and preset selection for stepper docs

--- packages/components/docs/StepperDocs.js
+++ packages/components/docs/StepperDocs.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
 import { Stepper, Select } from '../src';
 
+const presets = [
+  {
+    label: 'Simple',
+    value: 'simple',
+    steps: [
+      { label: 'Amount' },
+      { label: 'My details' },
+      { label: 'Recipient' },
+      { label: 'Done' },
+    ],
+  },
+  {
+    label: 'With actions',
+    value: 'with-actions',
+    steps: [
+      {
+        label: 'Clickable step 1',
+        onClick() {
+          alert('You clicked on step 1!'); // eslint-disable-line
+        },
+      },
+      {
+        label: 'Clickable step 2',
+        onClick() {
+          alert('You clicked on step 2!'); // eslint-disable-line
+        },
+      },
+      { label: 'Recipient' },
+      { label: 'Smellification' },
+      { label: 'Done' },
+    ],
+  },
+];
+
 export default class StepperDocs extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      steps: [
-        { label: 'Amount' },
-        { label: 'My details' },
-        { label: 'Recipient' },
-        { label: 'Done' },
-      ],
+      preset: presets[0],
       activeStep: 1,
     };
+  }
+
+  getStringifiedSteps() {
+    return JSON.stringify(
+      this.state.preset.steps.map(step =>
+        Object.keys(step)
+          .map(key => ({
+            key,
+            value: typeof step[key] === 'function' ? '[a function]' : step[key],
+          }))
+          .reduce((accumulator, current) => ({ ...accumulator, [current.key]: current.value }), {}),
+      ),
+      null,
+      '  ',
+    );
   }
 
   render() {
@@ -24,7 +68,7 @@ export default class StepperDocs extends Component {
             <p>These boots are made for steppin&lsquo;</p>
           </div>
           <div className="col-md-6">
-            <Stepper steps={this.state.steps} activeStep={this.state.activeStep} />
+            <Stepper steps={this.state.preset.steps} activeStep={this.state.activeStep} />
           </div>
         </div>
         <div className="row">
@@ -33,7 +77,7 @@ export default class StepperDocs extends Component {
             <pre className="tw-docs-code">
               {`<Stepper
   activeStep={${this.state.activeStep}}
-  steps={${JSON.stringify(this.state.steps, null, '  ')}}
+  steps={${this.getStringifiedSteps()}}
 />`}
             </pre>
             {/* eslint-enable react/jsx-indent */}
@@ -42,9 +86,19 @@ export default class StepperDocs extends Component {
             <label htmlFor="stepper-step-select">Active step</label>
             <Select
               id="stepper-step-select"
-              options={this.state.steps.map((_, index) => ({ label: index, value: index }))}
+              options={this.state.preset.steps.map((_, index) => ({ label: index, value: index }))}
               onChange={value => value && this.setState({ activeStep: value.value })}
               selected={{ label: this.state.activeStep, value: this.state.activeStep }}
+              required
+            />
+
+            <div className="m-t-3" />
+            <label htmlFor="stepper-preset-select">Step preset</label>
+            <Select
+              id="stepper-preset-select"
+              options={presets}
+              onChange={preset => preset && this.setState({ preset })}
+              selected={this.state.preset}
               required
             />
           </div>
