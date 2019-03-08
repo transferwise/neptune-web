53bce7565dab75c895c7c0866cb9c583838d07e9
Naiara Abaroa <naiara.abaroa@transferwise.com>
2019-03-08 09:16:55 +0000

    Update badge

--- packages/neptune-css/docs/BadgeDocs.js
+++ packages/neptune-css/docs/BadgeDocs.js
@@ -4,10 +4,37 @@ class Badge extends Component {
   render() {
     return (
       <section id="badge" className="wrap--content section">
-        <h1 className="colored-dot">
-          Badge <span className="badge">BT</span>
-        </h1>
-        <p className="lead">7kB minified</p>
+        <h1 className="colored-dot">Badge</h1>
+        <p className="lead">703 bytes</p>
+        <p className="lead">
+Easily highlight new or unread items by adding a
+          <code>{' <span class="badge">'}</code>
+          {' '}
+to links, Bootstrap navs, and more.
+        </p>
+        <div className="doc-example">
+          <a href="#">
+            Inbox&nbsp;
+            <span className="badge">42</span>
+          </a>
+          <code>{'<a href="#">Inbox <span class="badge">42</span></a>'}</code>
+        </div>
+
+        <h2 id="color">Color</h2>
+        <p>
+          Use
+          <code>.badge-success</code>
+          {' '}
+          contextual class.
+        </p>
+        <div className="doc-example" data-example-id="badges-styles">
+          <a href="#">
+            Inbox&nbsp;
+            <span className="badge badge-success">new</span>
+          </a>
+          <code>{'<a href="#">Inbox <span class="badge badge-success">new</span></a>'}</code>
+        </div>
+
       </section>
     );
   }
