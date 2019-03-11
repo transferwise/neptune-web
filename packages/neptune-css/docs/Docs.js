7a10b4d0c44b72860bba0e43b0b6876d97f88f2e
Naiara Abaroa <naiara.abaroa@transferwise.com>
2019-03-11 11:33:40 +0000

    Go to top arrow

--- packages/neptune-css/docs/Docs.js
+++ packages/neptune-css/docs/Docs.js
@@ -35,7 +35,12 @@ export default class Docs extends Component {
     return (
       <StrictMode>
         <div className="doc" id="top">
-          <a href="#top" className="doc-go-top">Go to top</a>
+          <a href="#top" className="doc-go-top">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24">
+              <title>up-arrow</title>
+              <polygon fill="#2e4369" points="18.01 7.47 12 1.46 5.99 7.47 6.7 8.18 11.5 3.38 11.5 22 12.5 22 12.5 3.38 17.3 8.18 18.01 7.47" />
+            </svg>
+          </a>
           <header className="wrap--content section">
             <h1 className="display-4 colored-dot">Thalassa</h1>
             <p className="lead">CSS library</p>
