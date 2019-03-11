99cacb49804319a067c186b5a308dfb3369afd3b
Carlos Matallín <carlos.matallin@transferwise.com>
2019-03-11 12:42:00 +0000

    Merge branch 'master' of github.com:transferwise/thalassa

--- packages/neptune-css/docs/MediaDocs.js
+++ packages/neptune-css/docs/MediaDocs.js
@@ -1,16 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Media extends Component {
-  render() {
-    return (
-      <section id="media" className="wrap--content section">
-        <h1 className="colored-dot">
-          Media
-        </h1>
-        <p>403 bytes</p>
-      </section>
-    );
-  }
-}
+const Media = () => (
+  <section id="media" className="wrap--content section">
+    <h1 className="colored-dot">
+        Media
+    </h1>
+    <p>403 bytes</p>
+  </section>
+  );
 
 export default Media;
