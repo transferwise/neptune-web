d42bdd965c232e3117428eccefbb1e7cfc7b55a0
OFFLlNE <valjalauri@gmail.com>
2019-05-03 10:24:12 +0100

    Add type check to deviceDetection

--- packages/components/src/stepper/deviceDetection.js
+++ packages/components/src/stepper/deviceDetection.js
@@ -1,8 +1,10 @@
 function supportsTouchEvents() {
   return !!(
-    window.ontouchstart !== undefined ||
-    navigator.maxTouchPoints ||
-    (window.DocumentTouch && document instanceof window.DocumentTouch)
+    (typeof window !== 'undefined' && window.ontouchstart !== undefined) ||
+    (typeof navigator !== 'undefined' && navigator.maxTouchPoints) ||
+    (typeof window !== 'undefined' &&
+      window.DocumentTouch &&
+      document instanceof window.DocumentTouch)
   );
 }
 
@@ -18,7 +20,7 @@ function userAgentSuggestsTouchDevice() {
   ];
   const matchString = sampleTouchDevices.map(device => `(${device})`).join('|');
   const regex = new RegExp(matchString, 'ig');
-  return !!navigator.userAgent.match(regex);
+  return typeof navigator !== 'undefined' && !!navigator.userAgent.match(regex);
 }
 // Important: this is not fool-proof! It gives false positives and negatives, and will be outdated.
 // Only use this for small vanity changes where it still works either way.
