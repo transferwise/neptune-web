b5ae3583ce15691a48d669b18eb4a99f1c494740
Henrique Gusso <henrique@gus.so>
2019-10-17 10:18:47 +0100

    Fix references to neptune-docs (#23)

--- packages/docs/utils/getBasePath.js
+++ packages/docs/utils/getBasePath.js
@@ -1,4 +1,4 @@
-const basePath = process.env.NODE_ENV === 'production' ? '/neptune-docs' : '';
+const basePath = process.env.NODE_ENV === 'production' ? '/neptune' : '';
 
 export default function getBasePath(path) {
   return `${basePath}${path}`;
