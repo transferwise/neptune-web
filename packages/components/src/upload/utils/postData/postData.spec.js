0ec8199a187790daa82118e0191883b936e62765
Marton Czebe <marton.czebe@transferwise.com>
2019-12-06 13:34:34 +0100

    test(upload): test postData error case

--- packages/components/src/upload/utils/postData/postData.spec.js
+++ packages/components/src/upload/utils/postData/postData.spec.js
@@ -17,12 +17,34 @@ describe('postData', () => {
     await expect(postData(HTTPOPTIONS, DATA)).resolves.toEqual(RESOLVE_RESPONSE);
   });
 
-  it('should work with reject', async () => {
+  it('should throw when call fails', async () => {
     const REJECT_RESPONSE = { status: '500', statusText: 'Rejected' };
     global.fetch = jest.fn(() => Promise.reject(REJECT_RESPONSE));
 
     expect.assertions(1);
     await expect(postData(HTTPOPTIONS, DATA)).rejects.toEqual(REJECT_RESPONSE);
+  });
+
+  it('should throw an Error when API returns an error code', async () => {
+    expect.assertions(1);
+
+    const ERROR_RESPONSE = { status: '500', statusText: 'Internal server error', ok: false };
+    global.fetch = jest.fn(() => Promise.resolve(ERROR_RESPONSE));
+
+    await expect(postData(HTTPOPTIONS, DATA)).rejects.toEqual(expect.any(Error));
+  });
+
+  it('should include the full response in the Error thrown', async () => {
+    expect.assertions(1);
+
+    const ERROR_RESPONSE = { status: '500', statusText: 'Internal server error', ok: false };
+    global.fetch = jest.fn(() => Promise.resolve(ERROR_RESPONSE));
+
+    try {
+      await postData(HTTPOPTIONS, DATA);
+    } catch (error) {
+      expect(error.response).toEqual(ERROR_RESPONSE);
+    }
   });
 
   it('should pass additional form data to request body', () => {
@@ -35,7 +57,7 @@ describe('postData', () => {
     expect(mockFetch.mock.calls[0][1].body).toEqual(data);
   });
 
-  it('should override Content-type and add any custom headers to the request', () => {
+  it('should override `Content-type` and add any custom headers to the request', () => {
     const CUSTOM_CONTENT_TYPE = 'foo';
     const CUSTOM_LANGUAGE = 'hu';
 
@@ -52,5 +74,14 @@ describe('postData', () => {
 
     expect(mockFetch.mock.calls[0][1].headers['Content-type']).toEqual(CUSTOM_CONTENT_TYPE);
     expect(mockFetch.mock.calls[0][1].headers['Accept-language']).toEqual(CUSTOM_LANGUAGE);
+  });
+
+  it('should set method to `POST`', () => {
+    const mockFetch = jest.fn(() => Promise.resolve({ ok: true }));
+    global.fetch = mockFetch;
+
+    postData(HTTPOPTIONS, DATA);
+
+    expect(mockFetch.mock.calls[0][1].method).toEqual('POST');
   });
 });
