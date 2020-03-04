b7a3a1ef703b4884e69d81ae3cada375fcae94e0
Steve Pole <StevePole@users.noreply.github.com>
2020-03-04 14:09:22 +0000

    docs: add storybook for Json Schema Form

--- packages/components/src/jsonSchemaForm/JsonSchemaForm.js
+++ packages/components/src/jsonSchemaForm/JsonSchemaForm.js
@@ -11,12 +11,35 @@ const JsonSchemaForm = props => {
 };
 
 JsonSchemaForm.propTypes = {
+  /**
+   * The JSON schema we're trying to satisfy
+   */
   schema: Types.shape({}).isRequired,
+  /**
+   * An initial data model
+   */
   model: Types.oneOfType([Types.string, Types.number, Types.bool, Types.array, Types.shape({})]),
+  /**
+   * Errors should be human readable strings, the object should match the shape of the model.
+   */
   errors: Types.oneOfType([Types.string, Types.array, Types.shape({})]),
+  /**
+   * The user's locale, this will affect the rendering of some components e.g. dates.
+   */
   locale: Types.string,
+  /**
+   * Translations used to enhance components.
+   */
   translations: Types.shape({}),
+  /**
+   * Fires when the internal data model changes as a result of form interaction.
+   *
+   * Provide two params the new model, and a boolean indicating if it's valid.
+   */
   onChange: Types.func.isRequired,
+  /**
+   * Tell the form if it has been submitted, triggering display of any validation failures.
+   */
   submitted: Types.bool.isRequired,
 };
 
