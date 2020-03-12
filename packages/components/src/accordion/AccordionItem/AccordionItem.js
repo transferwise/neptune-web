01a5d1fbb5d73a0eab32968031b4925b3a4c9ea1
Yi Zhang <yi.zhang@transferwise.com>
2020-03-12 14:23:27 -0400

    fix: register expand/collapse only on Accordion's title

--- packages/components/src/accordion/AccordionItem/AccordionItem.js
+++ packages/components/src/accordion/AccordionItem/AccordionItem.js
@@ -4,24 +4,22 @@ import classNames from 'classnames';
 import Chevron from '../../chevron';
 
 const AccordionItem = ({ title, content, onClick, isOpen, index }) => (
-  <label
-    onClick={() => onClick(index)}
-    className={classNames('tw-accordion-item decision p-a-0', { closed: !isOpen })}
-    htmlFor="accordion-chevron"
-  >
-    <div className="media p-y-3" id="accordion-chevron">
-      <div className="media-body">{typeof title === 'string' ? <h5>{title}</h5> : title}</div>
-      <div className="media-right media-middle">
-        <Chevron
-          size={Chevron.Size.SMALL}
-          orientation={Chevron.Orientation.BOTTOM}
-          flip={isOpen}
-          animate
-        />
+  <div className={classNames('tw-accordion-item decision p-a-0', { closed: !isOpen })}>
+    <label onClick={() => onClick(index)} className="tw-accordion-item" htmlFor="accordion-chevron">
+      <div className="media p-y-3" id="accordion-chevron">
+        <div className="media-body">{typeof title === 'string' ? <h5>{title}</h5> : title}</div>
+        <div className="media-right media-middle">
+          <Chevron
+            size={Chevron.Size.SMALL}
+            orientation={Chevron.Orientation.BOTTOM}
+            flip={isOpen}
+            animate
+          />
+        </div>
       </div>
-    </div>
+    </label>
     <div className="p-b-3 accordion-content">{content}</div>
-  </label>
+  </div>
 );
 
 AccordionItem.propTypes = {
