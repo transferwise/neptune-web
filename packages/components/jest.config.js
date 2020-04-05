e42af674f587d080a82dda17886641b75362084a
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2020-04-05 22:16:11 +0100

    test: moved jest conf inside jest.config

--- packages/components/jest.config.js
+++ packages/components/jest.config.js
@@ -4,6 +4,8 @@
  * @property {string} [testEnvironment] - Simulates browser in node environment since most of the tests rely in browser API.
  * @property {string} [moduleNameMapper] - Resolves all import of css to the specified file.
  * @property {array} [setupFilesAfterEnv] -Import the files specified after setting the env. extend-expect add some nice assertionprovided by react etsting library such as toHaveTextContent
+ * @property {array} [transformIgnorePatterns] - Do not transform anything outside src.
+ * @property {array} [testPathIgnorePatterns] - Ignore everything outside src.
  *
  * */
 
@@ -14,4 +16,7 @@ module.exports = {
     '\\.css$': require.resolve('./config/jest/style-mock.js'),
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  transformIgnorePatterns: ['^((?!src).)*$'],
+  testPathIgnorePatterns: ['^((?!src).)*$'],
+  testURL: 'http://localhost',
 };
