ea553f4d47bb91dd193e4a90919b26d7a81fcde3
Antti Onttonen <antti.onttonen@transferwise.com>
2020-05-15 17:26:28 +0100

    chore: better storybook theming (#366)

--- packages/components/.storybook/common/theme.js
+++ packages/components/.storybook/common/theme.js
@@ -1,39 +1,39 @@
 import { create } from '@storybook/theming';
 
-import Logo from './logo.svg';
+import Logo from './TransferWise.svg';
 
 export default create({
-  base: 'dark',
+  base: 'light',
 
   colorPrimary: '#37517e',
   colorSecondary: '#00b9ff',
 
   // UI
   appBg: '#f2f5f7',
-  appContentBg: 'silver',
-  appBorderColor: 'grey',
-  appBorderRadius: 4,
+  appContentBg: '#f2f5f7',
+  appBorderColor: '#e2e6e8',
+  appBorderRadius: 3,
 
   // Typography
-  fontBase: '"Averta", "Avenir W02", "Avenir", Helvetica, Arial, sans-serif',
-  fontCode: 'monospace',
+  // fontBase: '"Averta", "Avenir W02", "Avenir", Helvetica, Arial, sans-serif',
+  // fontCode: 'monospace',
 
   // Text colors
-  textColor: 'black',
-  textInverseColor: 'rgba(255,255,255,0.9)',
+  textColor: '#4a5860',
+  textInverseColor: 'rgba(255, 255, 255, 0.9)',
 
   // Toolbar default and active colors
-  barTextColor: 'silver',
-  barSelectedColor: 'black',
-  barBg: '#37517e',
+  barTextColor: '#ffffff',
+  barSelectedColor: '#00b9ff',
+  barBg: '#253655',
 
   // Form colors
-  inputBg: 'white',
-  inputBorder: 'silver',
-  inputTextColor: 'black',
-  inputBorderRadius: 4,
+  inputBg: '#ffffff',
+  inputBorder: '#e2e6e8',
+  inputTextColor: '#2e4369',
+  inputBorderRadius: 3,
 
-  brandTitle: 'Transferwise',
+  brandTitle: 'TransferWise',
   brandUrl: 'https://transferwise.com',
   brandImage: Logo,
 
