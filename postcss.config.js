ab0d0fd7cf24eab48eb02a928527d822605bcbb2
Antti Onttonen <antti.onttonen@transferwise.com>
2020-07-10 11:04:13 +0100

    fix: replace bootstrap variables with neptune tokens (#452)
    * fix: use neptune variables
    
    * feat: use forked postcss-custom-properties to provide fallbacks to custom props
    
    * fix: remove unused size variables
    
    * fix: use spacer variable
    
    * fix: import tokens from neptune-tokens
    
    * fix: remove variable import from summary
    
    * fix: correct imports
    
    * fix: use neptune-tokens package
    
    * fix: add z-index only for opened typeahead (#468)
    
    * refactor: use neptune variables
    
    * fix: import tokens from neptune-tokens
    
    * fix: remove variable import from summary
    
    * fix: correct imports
    
    * fix: use neptune-tokens package

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 require('postcss-import');
 require('autoprefixer');
-require('postcss-custom-properties');
+require('@arshaw/postcss-custom-properties');
 require('postcss-custom-media');
 require('cssnano');
 
@@ -8,7 +8,13 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     autoprefixer: {},
-    'postcss-custom-properties': {},
+    '@arshaw/postcss-custom-properties': {
+      importFrom: [
+        '../../node_modules/@transferwise/neptune-tokens/colors.css',
+        '../../node_modules/@transferwise/neptune-tokens/spacing.css',
+      ],
+      preserveWithFallback: true,
+    },
     'postcss-custom-media': {
       stage: 1,
     },
