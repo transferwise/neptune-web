639e1c4cf1d9f4e7a21acf2ff7970fb9e5451fa1
andreapiras <16908937+andreapiras@users.noreply.github.com>
2020-07-21 16:55:05 +0200

    feat: value normalizer (#485)

---
+++ packages/dynamic-flows/src/WithNormalizer/WithNormalizer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { normalizeEvent } from './validation/event-utilities';
+
+const methodsToExtend = ['onChange', 'onBlur', 'onFocus', 'onSuccess', 'onFailure'];
+/**
+ *
+ * This function is a temporary function used to normalize the returned values from our form related components. Please DO NOT USE THIS COMPONENT.
+ */
+const WithNormalizer = ({ children }) => {
+  const handleChildEvent = (childHandler) => (event) => {
+    const value = normalizeEvent(event, children.props.type);
+
+    childHandler(value);
+  };
+
+  const eventHandlers = {};
+
+  methodsToExtend.forEach((method) => {
+    if (children.props[method]) {
+      eventHandlers[method] = handleChildEvent(children.props[method]);
+    }
+  });
+
+  return React.cloneElement(children, {
+    ...eventHandlers,
+  });
+};
+
+export default WithNormalizer;
