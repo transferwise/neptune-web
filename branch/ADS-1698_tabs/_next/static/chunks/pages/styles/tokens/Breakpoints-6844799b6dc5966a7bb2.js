e927377e1a83490db8c58f5393d4ec2339dde86a
CircleCI <circleci@transferwise.com>
2020-11-18 11:21:38 +0000

    Updates

---
+++ branch/ADS-1698_tabs/_next/static/chunks/pages/styles/tokens/Breakpoints-6844799b6dc5966a7bb2.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[95],{Rris:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/styles/tokens/Breakpoints",function(){return o("ou/0")}])}},[["Rris",0,1,2,6,3,5,7,8,9,10,12,11,13,15,14,17,16,18,19,22,23,20,21,24,25,4]]]);
