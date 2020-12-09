13b842a028e29bc399728f270a400819c7823487
CircleCI <circleci@transferwise.com>
2020-12-09 18:08:04 +0000

    Updates

---
+++ branch/i18y/_next/static/chunks/36.b55221ec19bea44861bf.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[36],{rTno:function(a){a.exports=JSON.parse('{"np.CloseButton.aria-label":"Close","np.Pagination.aria-label":"Pagination navigation","np.PaginationLink.ariaLabel.active":"Go to page {pageNumber}","np.PaginationLink.ariaLabel.inactive":"Current page, page {pageNumber}"}')}}]);
