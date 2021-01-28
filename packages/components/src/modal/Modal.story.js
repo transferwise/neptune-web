a63d2a488a48622499e6e78f281409e2e1af7919
Helen Durrant <helen.durrant@transferwise.com>
2021-01-28 13:20:59 +1300

    fix: add component animations back into neptune and update the modal for ease of testing

--- packages/components/src/modal/Modal.story.js
+++ packages/components/src/modal/Modal.story.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { boolean, select, text } from '@storybook/addon-knobs';
+import { select, text } from '@storybook/addon-knobs';
 import { Button, Modal, DateInput } from '..';
 
 export default {
@@ -9,44 +9,40 @@ export default {
 
 export const basic = () => {
   const size = select('size', Object.values(Modal.Size), Modal.Size.MEDIUM);
-  const open = boolean('open', true);
+  const [open, setOpen] = React.useState(false);
   const title = text('title', 'title');
   const scroll = select('scroll', Object.values(Modal.Scroll), Modal.Scroll.CONTENT);
   const position = select('position', Object.values(Modal.Position), Modal.Position.CENTER);
 
   return (
     <>
+      <Button onClick={() => setOpen(true)}>Open Modal</Button>
       <Modal
         body={
           <>
-            <p className="m-t-4">
-              I am baby actually poke kickstarter, street art jean shorts bespoke chambray activated
-              charcoal ramps marfa shoreditch tumeric tumblr. Mixtape cred palo santo, cliche lyft
-              marfa ethical waistcoat poke jean shorts. Scenester readymade selvage disrupt pok pok.
-              La croix stumptown try-hard chartreuse. I am baby actually poke kickstarter, street
-              art jean shorts bespoke chambray activated charcoal ramps marfa shoreditch tumeric I
-              am baby actually poke kickstarter, street art jean shorts bespoke chambray activated
-              charcoal ramps marfa shoreditch tumeric tumblr. Mixtape cred palo santo, cliche lyft
-              marfa ethical waistcoat poke jean shorts. Scenester readymade selvage disrupt pok pok.
-              La croix stumptown try-hard chartreuse. I am baby actually poke kickstarter, street
-              art jean shorts bespoke chambray activated charcoal ramps marfa shoreditch tumeric
-            </p>
             <DateInput onChange={console && console.log} value={new Date()} />
+            <p className="m-t-4">
+              I'm baby actually poke kickstarter, street art jean shorts bespoke chambray activated
+              charcoal ramps marfa shoreditch tumeric tumblr. Mixtape cred palo santo, cliche lyft
+              marfa ethical waistcoat poke jean shorts. Scenester readymade selvage disrupt pok pok.
+              La croix stumptown try-hard chartreuse. I'm baby actually poke kickstarter, street art
+              jean shorts bespoke chambray activated charcoal ramps marfa shoreditch tumeric
+            </p>
           </>
         }
         open={open}
         scroll={scroll}
         position={position}
-        onClose={() => {
-          console.log('close modal');
-        }}
+        onClose={() => setOpen(false)}
         size={size}
         title={title}
+        className=""
         footer={
           <Button block onClick={() => alert('clicked')}>
             Action
           </Button>
         }
+        closeOnClick
       />
     </>
   );
