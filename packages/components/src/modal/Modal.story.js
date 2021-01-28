f46f046ca487e6acd5d0d90cfccb2c2d7b69bbc1
Helen Durrant <61203573+helendurranttw@users.noreply.github.com>
2021-01-29 04:54:27 +1300

    fix: add component animations back into neptune (#774)

--- packages/components/src/modal/Modal.story.js
+++ packages/components/src/modal/Modal.story.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
 import React from 'react';
-import { boolean, select, text } from '@storybook/addon-knobs';
+import { select, text } from '@storybook/addon-knobs';
 import { Button, Modal, DateInput } from '..';
 
 export default {
@@ -9,44 +11,40 @@ export default {
 
 export const basic = () => {
   const size = select('size', Object.values(Modal.Size), Modal.Size.MEDIUM);
-  const open = boolean('open', true);
+  const [open, setOpen] = React.useState(false);
   const title = text('title', 'title');
   const scroll = select('scroll', Object.values(Modal.Scroll), Modal.Scroll.CONTENT);
   const position = select('position', Object.values(Modal.Position), Modal.Position.CENTER);
 
   return (
     <>
+      <Button onClick={() => setOpen(true)}>Open Modal</Button>
       <Modal
         body={
           <>
+            <DateInput onChange={console && console.log} value={new Date()} />
             <p className="m-t-4">
               I am baby actually poke kickstarter, street art jean shorts bespoke chambray activated
               charcoal ramps marfa shoreditch tumeric tumblr. Mixtape cred palo santo, cliche lyft
               marfa ethical waistcoat poke jean shorts. Scenester readymade selvage disrupt pok pok.
               La croix stumptown try-hard chartreuse. I am baby actually poke kickstarter, street
-              art jean shorts bespoke chambray activated charcoal ramps marfa shoreditch tumeric I
-              am baby actually poke kickstarter, street art jean shorts bespoke chambray activated
-              charcoal ramps marfa shoreditch tumeric tumblr. Mixtape cred palo santo, cliche lyft
-              marfa ethical waistcoat poke jean shorts. Scenester readymade selvage disrupt pok pok.
-              La croix stumptown try-hard chartreuse. I am baby actually poke kickstarter, street
               art jean shorts bespoke chambray activated charcoal ramps marfa shoreditch tumeric
             </p>
-            <DateInput onChange={console && console.log} value={new Date()} />
           </>
         }
         open={open}
         scroll={scroll}
         position={position}
-        onClose={() => {
-          console.log('close modal');
-        }}
+        onClose={() => setOpen(false)}
         size={size}
         title={title}
+        className=""
         footer={
           <Button block onClick={() => alert('clicked')}>
             Action
           </Button>
         }
+        closeOnClick
       />
     </>
   );
