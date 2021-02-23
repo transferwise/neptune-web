11ca9b585c0b9a254bab5e4f9cf7dc8ccc5eeb59
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2021-02-23 13:26:02 +0000

    chore: cleaned up code

--- packages/components/src/common/panel/Panel.js
+++ packages/components/src/common/panel/Panel.js
@@ -8,45 +8,47 @@ import './Panel.css';
 
 const POPOVEROFFSET = [0, 16];
 
-const Panel = forwardRef(({ arrow, children, className, open, position, triggerRef }, ref) => {
-  const [arrowElement, setArrowElement] = useState(null);
-  const [popperElement, setPopperElement] = useState(null);
+const Panel = forwardRef(
+  ({ arrow, children, className, open, position: placement, triggerRef }, ref) => {
+    const [arrowElement, setArrowElement] = useState(null);
+    const [popperElement, setPopperElement] = useState(null);
 
-  const modifiers = [];
+    const modifiers = [];
 
-  if (arrow) {
-    modifiers.push({ name: 'arrow', options: { padding: 5, element: arrowElement } });
-    // This lets you displace a popper element from its reference element.
-    modifiers.push({ name: 'offset', options: { offset: POPOVEROFFSET } });
-  }
+    if (arrow) {
+      modifiers.push({ name: 'arrow', options: { element: arrowElement } });
+      // This lets you displace a popper element from its reference element.
+      modifiers.push({ name: 'offset', options: { offset: POPOVEROFFSET } });
+    }
 
-  const { styles, attributes } = usePopper(triggerRef.current, popperElement, {
-    placement: position,
-    modifiers,
-  });
+    const { styles, attributes } = usePopper(triggerRef.current, popperElement, {
+      placement,
+      modifiers,
+    });
 
-  return (
-    // Popper recommends to use the popper element as a wrapper around an inner element that can have any CSS property transitioned for animations.
-    <div
-      ref={setPopperElement}
-      style={{ ...styles.popper }}
-      {...attributes.popper}
-      className={classnames('np-panel', { 'np-panel--open': open }, className)}
-    >
-      <div ref={ref} className={classnames('np-panel__content')}>
-        {children}
-        {/* Arrow has to stay inside content to get the same animations as the "dialog" and to get hidden when panel is closed. */}
-        {arrow && (
-          <div
-            className={classnames('np-panel__arrow')}
-            ref={setArrowElement}
-            style={styles.arrow}
-          />
-        )}
+    return (
+      // Popper recommends to use the popper element as a wrapper around an inner element that can have any CSS property transitioned for animations.
+      <div
+        ref={setPopperElement}
+        style={{ ...styles.popper }}
+        {...attributes.popper}
+        className={classnames('np-panel', { 'np-panel--open': open }, className)}
+      >
+        <div ref={ref} className={classnames('np-panel__content')}>
+          {children}
+          {/* Arrow has to stay inside content to get the same animations as the "dialog" and to get hidden when panel is closed. */}
+          {arrow && (
+            <div
+              className={classnames('np-panel__arrow')}
+              ref={setArrowElement}
+              style={styles.arrow}
+            />
+          )}
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  },
+);
 
 Panel.Position = {
   BOTTOM: Position.BOTTOM,
