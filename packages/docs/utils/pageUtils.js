84a700e1a23bc4b1b08f8f7cef950eaf44a286a9
Anton Dozortsev <dozortsevanton@gmail.com>
2021-03-09 11:31:25 +0000

    chore: enable TypeScript in docs package (#815)

--- packages/docs/utils/pageUtils.js
+++ packages/docs/utils/pageUtils.js
@@ -2,6 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { format, formatDistance } from 'date-fns';
 
+/**
+ * Check if date is past date
+ *
+ * @param {Date | null | undefined} endDate test date
+ * @return {boolean} `true` if date is less than today or equal `null` / `undefined`,
+ * otherwise returns `false`
+ */
 export function isExpired(endDate) {
   return endDate?.getTime() <= Date.now();
 }
@@ -13,7 +20,7 @@ export function printDate(date) {
 }
 
 const getPages = () => {
-  const req = require.context('../pages/', true, /mdx$|js$/);
+  const req = require.context('../pages/', true, /mdx$|js$|tsx$/);
 
   return req.keys().map((filePath) => {
     // Chop off the './' and file extention
@@ -95,6 +102,9 @@ export const getFirstPageInSection = (section) => {
   return pages.find((p) => p.path.indexOf(dir) === 1);
 };
 
+/**
+ * @param {string} url
+ */
 export const addBasePath = (url) =>
   `${process.env.ASSET_PREFIX}/${url.indexOf('/') === 0 ? url.slice(1) : url}`;
 
