687d9598cca7f122b4489938777644dc6316439e
James Luu <james.luu@transferwise.com>
2021-03-11 17:03:27 +0000

    feat: Accordion changes

--- packages/components/src/accordion/AccordionItem/AccordionItem.js
+++ packages/components/src/accordion/AccordionItem/AccordionItem.js
@@ -1,41 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Types from 'prop-types';
 import classNames from 'classnames';
 import { isString } from '@transferwise/neptune-validation';
 import Chevron from '../../chevron';
 
-const AccordionItem = ({ id, title, content, onClick, isOpen, index }) => (
-  <div id={id} className={classNames('tw-accordion-item decision p-a-0', { closed: !isOpen })}>
-    <button
-      type="button"
-      aria-expanded={isOpen}
-      onClick={() => onClick(index)}
-      className="tw-accordion-item btn btn-link p-l-0 text-no-decoration"
-    >
-      <div className="media p-y-2">
-        <div className="media-body text-xs-left">
-          {isString(title) ? <span className="h5">{title}</span> : title}
+const AccordionItem = ({ id, title, content, onClick, isInitiallyOpen, icon }) => {
+  const [isOpen, setIsOpen] = useState(isInitiallyOpen);
+
+  return (
+    <div id={id} className={classNames('tw-accordion-item decision p-a-0', { closed: !isOpen })}>
+      <button
+        type="button"
+        aria-expanded={isOpen}
+        onClick={() => {
+          setIsOpen(!isOpen);
+          onClick();
+        }}
+        className="tw-accordion-item btn btn-link p-l-0 text-no-decoration"
+      >
+        <div className="media p-y-2">
+          {icon && <div className="media-left hidden-xs hidden-sm">{icon}</div>}
+          <div className="media-body text-xs-left">
+            {isString(title) ? <span className="h5">{title}</span> : title}
+          </div>
+          <div className="media-right media-middle">
+            <Chevron orientation={isOpen ? Chevron.Orientation.TOP : Chevron.Orientation.BOTTOM} />
+          </div>
         </div>
-        <div className="media-right media-middle">
-          <Chevron orientation={isOpen ? Chevron.Orientation.TOP : Chevron.Orientation.BOTTOM} />
+      </button>
+      <div className="p-b-3 accordion-content media">
+        {icon && (
+          <div className="media-left hidden-xs hidden-sm">
+            <div className="p-r-3" />
+          </div>
+        )}
+        <div className="media-body">{content}</div>
+        <div className="media-right">
+          <div className="p-l-3" />
         </div>
       </div>
-    </button>
-    <div className="p-b-3 accordion-content">{content}</div>
-  </div>
-);
+    </div>
+  );
+};
 
 AccordionItem.propTypes = {
   id: Types.string,
   title: Types.node.isRequired,
-  isOpen: Types.bool.isRequired,
   content: Types.node.isRequired,
   onClick: Types.func.isRequired,
-  index: Types.number.isRequired,
+  isInitiallyOpen: Types.bool.isRequired,
+  icon: Types.node,
 };
 
 AccordionItem.defaultProps = {
   id: null,
+  icon: null,
 };
 
 export default AccordionItem;
