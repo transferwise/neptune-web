8f7aeb8a678ea670b4ef15e7f66eb079584d9c42
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2021-03-15 11:39:39 +0000

    fix: onclick

--- packages/docs/liveEditorCode/circularButton.code.js
+++ packages/docs/liveEditorCode/circularButton.code.js
@@ -1,7 +1,7 @@
 <CircularButton
   icon={<Freeze />}
   type={CircularButton.Type.ACCENT}
-  onClick={console.log('Button clicked')}
+  onClick={() => console.log('Button clicked')}
 >
   Freeze
 </CircularButton>;
