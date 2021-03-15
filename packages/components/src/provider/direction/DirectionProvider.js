f10d6b45c64f70bb60146f3e08ac1021020d1c5c
joe-sayer <sayerj97@gmail.com>
2021-03-15 12:13:51 +0000

    chore: changed default value of DirectionContext

--- packages/components/src/provider/direction/DirectionProvider.js
+++ packages/components/src/provider/direction/DirectionProvider.js
@@ -3,7 +3,7 @@ import Types from 'prop-types';
 
 import { DEFAULT_LOCALE, SUPPORTED_RTL } from '../../common/locale';
 
-export const DirectionContext = React.createContext(false);
+export const DirectionContext = React.createContext('ltr');
 
 export const DirectionProvider = ({ locale, children }) => (
   <DirectionContext.Provider value={SUPPORTED_RTL.indexOf(locale) > -1 ? 'rtl' : 'ltr'}>
