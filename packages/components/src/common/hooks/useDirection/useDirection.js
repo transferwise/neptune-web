0000a694f0e267b9712eb5281e2ddf6db1b294cd
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2021-03-15 20:45:08 +0000

    chore: update tests

--- packages/components/src/common/hooks/useDirection/useDirection.js
+++ packages/components/src/common/hooks/useDirection/useDirection.js
@@ -1,4 +1,7 @@
 import { useContext } from 'react';
 import { DirectionContext } from '../../../provider/direction/DirectionProvider';
 
-export const useDirection = () => ({direction: useContext(DirectionContext), isRTL: useContext(DirectionContext) === 'rtl'})
+export const useDirection = () => ({
+  direction: useContext(DirectionContext),
+  isRTL: useContext(DirectionContext) === 'rtl',
+});
--- packages/components/src/common/hooks/useDirection/useDirection.spec.js
+++ packages/components/src/common/hooks/useDirection/useDirection.spec.js
@@ -1,12 +1,36 @@
+import React from 'react';
 import { renderHook } from '@testing-library/react-hooks';
 import { useDirection } from './useDirection';
 
+let realUseContext;
+let useContextMock;
+let direction = 'ltr';
+
+beforeEach(() => {
+  realUseContext = React.useContext;
+  React.useContext = jest.fn(() => direction);
+  useContextMock = React.useContext;
+});
+// Cleanup mock
+afterEach(() => {
+  React.useContext = realUseContext;
+});
 describe('useRTL', () => {
-  it('returns 0', () => {
+  it(`returns direction:'ltr' and isRTL false when context is ltr`, () => {
     const {
       result: { current },
     } = renderHook(() => useDirection());
 
-    expect(current).toEqual({ direction: 'ltr', isRTL: false });
+    expect(current).toEqual({ direction, isRTL: false });
+  });
+
+  it(`returns direction:'rtl' and isRTL true when context is rtl`, () => {
+    direction = 'rtl';
+    useContextMock.mockReturnValue(direction);
+    const {
+      result: { current },
+    } = renderHook(() => useDirection());
+
+    expect(current).toEqual({ direction, isRTL: true });
   });
 });
