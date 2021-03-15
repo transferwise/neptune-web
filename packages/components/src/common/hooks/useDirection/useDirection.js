e52a56c6045050b23da2d651ce11c54ace4aa32a
joe-sayer <sayerj97@gmail.com>
2021-03-15 11:52:12 +0000

    chore: partial

--- packages/components/src/common/hooks/useDirection/useDirection.js
+++ packages/components/src/common/hooks/useDirection/useDirection.js
@@ -1,4 +1,4 @@
 import { useContext } from 'react';
 import { DirectionContext } from '../../../provider/direction/DirectionProvider';
 
-export const useDirection = () => [useContext(DirectionContext)];
+export const useDirection = () => ({direction: useContext(DirectionContext), isRTL: useContext(DirectionContext) === 'rtl'})
--- packages/components/src/common/hooks/useDirection/useDirection.spec.js
+++ packages/components/src/common/hooks/useDirection/useDirection.spec.js
@@ -1,12 +1,12 @@
 import { renderHook } from '@testing-library/react-hooks';
 import { useDirection } from './useDirection';
 
-describe('useDirection', () => {
+describe('useRTL', () => {
   it('returns 0', () => {
     const {
       result: { current },
     } = renderHook(() => useDirection());
 
-    expect(current).toEqual([false]);
+    expect(current).toEqual({ direction: 'ltr', isRTL: false });
   });
 });
