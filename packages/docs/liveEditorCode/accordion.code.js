72af46701d5cfe109df5858a9bffc92219ab067b
OFFLlNE <lauri.valja@transferwise.com>
2021-03-16 18:04:54 +0200

    docs: update docs

--- packages/docs/liveEditorCode/accordion.code.js
+++ packages/docs/liveEditorCode/accordion.code.js
@@ -4,17 +4,17 @@
     {
       title: 'Item 1',
       content: 'I can be text',
-      icon: <Freeze />,
+      icon: <Freeze size={24} />,
     },
     {
       title: 'Item 2 with icon',
       content: <span>I can be a node</span>,
-      icon: <Emoji />,
+      icon: <Emoji size={24} />,
     },
     {
       title: 'Item 3',
       content: 'I can be text',
-      icon: <New />,
+      icon: <New size={24} />,
     },
   ]}
 />;
