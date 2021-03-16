7afbe5aa994a579e99c64346007747ea80c445a8
OFFLlNE <lauri.valja@transferwise.com>
2021-03-16 10:53:10 +0200

    feat: Accordion changes

--- packages/docs/liveEditorCode/accordion.code.js
+++ packages/docs/liveEditorCode/accordion.code.js
@@ -1,20 +1,22 @@
-import { FastFlag } from '@transferwise/icons';
-
-<Accordion
-  indexOpen={1}
-  items={[
-    {
-      title: 'Item 1',
-      content: 'I can be text',
-    },
-    {
-      title: 'Item 2 with icon',
-      content: <span>I can be a node</span>,
-      icon: <FastFlag />,
-    },
-    {
-      title: 'Item 3',
-      content: 'I can be text',
-    },
-  ]}
-/>;
+({ icon }) => {
+  return (
+    <Accordion
+      indexOpen={1}
+      items={[
+        {
+          title: 'Item 1',
+          content: 'I can be text',
+        },
+        {
+          title: 'Item 2 with icon',
+          content: <span>I can be a node</span>,
+          icon,
+        },
+        {
+          title: 'Item 3',
+          content: 'I can be text',
+        },
+      ]}
+    />
+  );
+};
