dd2772bd277611a0b10a4405668c06e011491e33
OFFLlNE <lauri.valja@transferwise.com>
2021-03-16 11:04:40 +0200

    feat: Accordion changes

--- packages/docs/liveEditorCode/accordion.code.js
+++ packages/docs/liveEditorCode/accordion.code.js
@@ -1,22 +1,18 @@
-({ icon }) => {
-  return (
-    <Accordion
-      indexOpen={1}
-      items={[
-        {
-          title: 'Item 1',
-          content: 'I can be text',
-        },
-        {
-          title: 'Item 2 with icon',
-          content: <span>I can be a node</span>,
-          icon,
-        },
-        {
-          title: 'Item 3',
-          content: 'I can be text',
-        },
-      ]}
-    />
-  );
-};
+<Accordion
+  indexOpen={1}
+  items={[
+    {
+      title: 'Item 1',
+      content: 'I can be text',
+    },
+    {
+      title: 'Item 2 with icon',
+      content: <span>I can be a node</span>,
+      icon: <FastFlag />,
+    },
+    {
+      title: 'Item 3',
+      content: 'I can be text',
+    },
+  ]}
+/>;
